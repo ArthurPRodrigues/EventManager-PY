7263877ff8109edab741cccaa73f2bd4b39a9eda
Arthur Rodrigues <arthurprodrigues1@gmail.com>
2025-09-13 16:45:03 -0300

    feat: add get_by_id in users repository

--- src/user/infra/persistence/sqlite_users_repository.py
+++ src/user/infra/persistence/sqlite_users_repository.py
@@ -4,7 +4,7 @@ from shared.infra.persistence.sqlite import SQLiteDatabase
 from user.domain.user import User
 
 
-class SqliteUserRepository:
+class SqliteUsersRepository:
     def __init__(self, db: SQLiteDatabase) -> None:
         self._db = db
 
@@ -21,3 +21,27 @@ class SqliteUserRepository:
                 ),
             )
             conn.commit()
+
+    def get_by_id(self, id: int) -> User | None:
+        with self._db.connect() as conn:
+            row = conn.execute(
+                """
+                SELECT id, name, email, hashed_password, role
+                FROM users
+                WHERE id = ?
+                """,
+                (id,),
+            ).fetchone()
+
+        if not row:
+            return None
+
+        user_id, name, email, hashed_password, role = row
+
+        return User(
+            id=user_id,
+            name=name,
+            email=email,
+            hashed_password=hashed_password,
+            role=role,
+        )
