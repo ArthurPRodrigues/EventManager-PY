de0871d1ee0a5511a5331ad314e8d26ea2f627f8
Arthur Rodrigues <arthurprodrigues1@gmail.com>
2025-09-15 21:35:02 -0300

    feat: add create_user_view

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
 from shared.composition_root import build_application
+from user.infra.presentation.create_user_view import CreateUserView
 
 
 def run() -> None:
     build_application()
+    CreateUserView()
 
 
 if __name__ == "__main__":
