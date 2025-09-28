5eb74c111ac9e8c80b5a0f39a5fdb3829d3564a5
Fábio Fernandes <dev.fabiofernandes@gmail.com>
2025-09-28 17:10:24 -0300

    refactor: update layout and styling in FriendshipPendingInvitesGUI for consistency

--- src/friendship/ui/friendship_pending_invites_gui.py
+++ src/friendship/ui/friendship_pending_invites_gui.py
@@ -1,5 +1,3 @@
-import FreeSimpleGUI as sg
-
 from friendship.application.accept_friendship_invite_use_case import (
     AcceptFriendshipInviteInputDto,
 )
@@ -9,13 +7,14 @@ from friendship.application.list_friendships_with_user_email_and_name_use_case i
 )
 from shared.ui.base_gui import BaseGUI
 from shared.ui.components import ActionButtonsComponent, HeaderComponent, TableComponent
+from shared.ui.styles import BUTTON_SIZES, COLORS, WINDOW_SIZES
 
 
 class FriendshipPendingInvitesGUI(BaseGUI):
     def __init__(self, use_cases=None, navigator=None, auth_context=None):
         super().__init__(
             title="Pending Invites",
-            size=(700, 400),
+            size=WINDOW_SIZES["HORIZONTAL_DEFAULT"],
             use_cases=use_cases,
             navigator=navigator,
             auth_context=auth_context,
@@ -24,7 +23,7 @@ class FriendshipPendingInvitesGUI(BaseGUI):
         self.header = HeaderComponent()
 
         self.table = TableComponent(
-            headers=["ID", "Name", "E-mail"],
+            headers=["ID", "NAME", "E-MAIL"],
             data_callback=self._load_friendships_callback,
             key="-TABLE-",
             items_per_page=10,
@@ -35,12 +34,14 @@ class FriendshipPendingInvitesGUI(BaseGUI):
             {
                 "text": "Accept Selected",
                 "key": "-ACCEPT_SELECTED-",
-                "size": (12, 1),
+                "button_color": (COLORS["white"], COLORS["success"]),
+                "size": BUTTON_SIZES["MEDIUM"],
             },
             {
                 "text": "Decline Selected",
                 "key": "-DECLINE_SELECTED-",
-                "size": (12, 1),
+                "button_color": (COLORS["white"], COLORS["warning"]),
+                "size": BUTTON_SIZES["MEDIUM"],
             },
         ])
 
@@ -52,12 +53,8 @@ class FriendshipPendingInvitesGUI(BaseGUI):
     def create_layout(self):
         layout = [
             *self.header.create_layout(),
-            [sg.HorizontalSeparator()],
-            [sg.Text("")],
             *self.table.create_layout(),
-            [sg.Text("")],
             *self.action_buttons.create_layout(),
-            [sg.Text("")],
         ]
 
         return layout
