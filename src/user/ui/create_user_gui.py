3e221f99cbe04ca6926ab7398305c44cddf44d8a
arthuPRodrigues <arthurprodrigues1@gmail.com>
2025-09-29 17:34:07 -0300

    fix: adjust size input name and email in create user gui

--- src/user/ui/create_user_gui.py
+++ src/user/ui/create_user_gui.py
@@ -47,39 +47,12 @@ class CreateUserGUI(BaseGUI):
                 ),
             ],
             [
-                sg.Text(
-                    "Email*",
-                    font=FONTS["LABEL"],
-                    size=LABEL_SIZES["DEFAULT"],
-                    pad=(0, 10),
-                ),
+                sg.Text("Name*", size=(12, 1)),
+                sg.Input(key="-NAME-"),
             ],
             [
-                sg.Text(
-                    "Password*",
-                    font=FONTS["LABEL"],
-                    size=LABEL_SIZES["DEFAULT"],
-                    pad=(0, 10),
-                ),
-            ],
-            [
-                sg.Text(
-                    "Role*",
-                    font=FONTS["LABEL"],
-                    size=LABEL_SIZES["DEFAULT"],
-                    pad=(0, 10),
-                ),
-            ],
-        ]
-
-        inputs = [
-            [
-                sg.Input(
-                    key="-NAME-",
-                    tooltip="Enter your full name",
-                    font=FONTS["INPUT"],
-                    pad=(0, 10),
-                ),
+                sg.Text("Email*", size=(12, 1)),
+                sg.Input(key="-EMAIL-"),
             ],
             [
                 sg.Input(
