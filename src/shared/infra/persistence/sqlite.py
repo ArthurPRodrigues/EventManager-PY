0a8655b3b85b4beac22375ff990886be17227479
arthuPRodrigues <arthurprodrigues1@gmail.com>
2025-10-04 23:25:16 -0300

    feat: add table ticket

--- src/shared/infra/persistence/sqlite.py
+++ src/shared/infra/persistence/sqlite.py
@@ -56,4 +56,19 @@ class SQLiteDatabase:
                     )
                 """
             )
+            conn.execute(
+                """
+                CREATE TABLE IF NOT EXISTS ticket (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    code TEXT NOT NULL,
+                    created_at TIMESTAMP NOT NULL,
+                    status TEXT NOT NULL,
+                    event_id INTEGER NOT NULL,
+                    client_id INTEGER NOT NULL,
+                    UNIQUE(code),
+                    FOREIGN KEY(event_id) REFERENCES events(id) ON DELETE CASCADE,
+                    FOREIGN KEY(client_id) REFERENCES users(id) ON DELETE CASCADE
+                )
+                """
+            )
             conn.commit()
