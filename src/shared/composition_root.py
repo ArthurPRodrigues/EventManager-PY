6f220edf9835c9af40bda57584790231d1fe269d
arthuPRodrigues <arthurprodrigues1@gmail.com>
2025-10-04 23:31:51 -0300

    fix: ticket in composition root

--- src/shared/composition_root.py
+++ src/shared/composition_root.py
@@ -18,6 +18,8 @@ from friendship.infra.persistence.sqlite_friendship_repository import (
     SqliteFriendshipRepository,
 )
 from shared.infra.persistence.sqlite import SQLiteDatabase
+from ticket.application.redeem_ticket_use_case import RedeemTicketUseCase
+from ticket.infra.persistence.sqlite_ticket_repository import SqliteTicketRepository
 from user.application.authenticate_user_use_case import AuthenticateUserUseCase
 from user.application.create_user_use_case import CreateUserUseCase
 from user.infra.persistence.sqlite_users_repository import SqliteUsersRepository
@@ -36,6 +38,8 @@ class CompositionRoot:
     authenticate_user_use_case: AuthenticateUserUseCase
     list_event_use_case: ListEventUseCase
     event_repo: SqliteEventRepository
+    ticket_repo: SqliteTicketRepository
+    redeem_ticket_use_case: RedeemTicketUseCase
 
 
 def build_application(db_path: str | None = None) -> CompositionRoot:
@@ -46,6 +50,7 @@ def build_application(db_path: str | None = None) -> CompositionRoot:
     friendship_repo = SqliteFriendshipRepository(db)
     user_repo = SqliteUsersRepository(db)
     event_repo = SqliteEventRepository(db)
+    ticket_repo = SqliteTicketRepository(db)
 
     # Use Cases
     send_friendship_invite_use_case = SendFriendshipInviteUseCase(
@@ -59,17 +64,20 @@ def build_application(db_path: str | None = None) -> CompositionRoot:
     create_user_use_case = CreateUserUseCase(user_repo)
     authenticate_user_use_case = AuthenticateUserUseCase(user_repo)
     list_event_use_case = ListEventUseCase(event_repo)
+    redeem_ticket_use_case = RedeemTicketUseCase(ticket_repo, event_repo, user_repo)
 
     return CompositionRoot(
         db=db,
+        friendship_repo=friendship_repo,
         send_friendship_invite_use_case=send_friendship_invite_use_case,
         accept_friendship_invite_use_case=accept_friendship_invite_use_case,
         delete_friendship_use_case=delete_friendship_use_case,
         list_friendships_use_case=list_friendships_use_case,
+        user_repo=user_repo,
         create_user_use_case=create_user_use_case,
         authenticate_user_use_case=authenticate_user_use_case,
-        list_event_use_case=list_event_use_case,
         event_repo=event_repo,
-        friendship_repo=friendship_repo,
-        user_repo=user_repo,
+        list_event_use_case=list_event_use_case,
+        ticket_repo=ticket_repo,
+        redeem_ticket_use_case=redeem_ticket_use_case,
     )
