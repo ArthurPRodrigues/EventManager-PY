cfdc92e05383826d8d3ebbb22a7b197c2b492511
arthuPRodrigues <arthurprodrigues1@gmail.com>
2025-10-05 20:47:43 -0300

    feat: eventDto

--- src/event/application/dtos.py
+++ src/event/application/dtos.py
@@ -1,6 +1,15 @@
-from event.domain.event import Event
+class EventDto:
+    name: str
+    location: str
+    created_at: str
+    start_date: str
+    end_date: str
+    tickets_available: int
+    organizer_id: int
+    staffs_id: list[str] | None
+    id: int | None = None
 
 
 class PaginatedEventsDto:
-    event_list: list[Event]
+    event_list: list[EventDto]
     total_event_count: int
